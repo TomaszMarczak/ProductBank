bc03bb116fd228346267d931e19189f73c306fc3
Tomasz Marczak <tommy.marczak@gmail.com>
2023-08-18 19:44:22 +0200

    Link to /

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -3,6 +3,7 @@ import { ModeToggle } from "./ThemeToggle";
 import SearchProductButton from "./SearchProductButton";
 import NavbarUserControl from "./NavbarUserControl";
 import { LayoutGroup, motion } from "framer-motion";
+import Link from "next/link";
 
 interface NavbarProps extends React.HTMLAttributes<HTMLElement> {}
 
@@ -10,9 +11,11 @@ const Navbar = (props: NavbarProps) => {
   return (
     <nav className="flex items-center justify-between flex-wrap p-4">
       <div className="flex items-center">
-        <span className="font-semibold text-xl transition-all ">
-          Product Library
-        </span>
+        <Link href="/">
+          <span className="font-semibold text-xl transition-all ">
+            Product Library
+          </span>
+        </Link>
       </div>
       <div className="ml-auto flex align-middle gap-2">
         <LayoutGroup>
